586017c12d0874965347e52e2d734476e9eb1d0b
Rayan Moarkech <rayanzone@hotmail.com>
2022-09-29 19:41:49 -0400

    Update Territory

--- include/Territory.h
+++ include/Territory.h
@@ -5,24 +5,30 @@
 #ifndef TERRITORY_H
 #define TERRITORY_H
 
-#include <iostream>
+#include <string>
+    using std::string;
 #include <vector>
+    using std::vector;
 
 #include "Continent.h"
+
+class Continent; // Used to determine size of the pointer
 
 class Territory
 {
 private:
     int coordinateX;
     int coordinateY;
-    std::string name;
+    string name;
     Continent* continent;
-    std::vector<std::string> adjacentTerritoryNames; // The maximum allowed is 10. Maybe change to array?
+    vector<Territory*> adjacentTerritories; // The maximum allowed is 10. Maybe change to array?
 //    Player* ownedBy; // TODO: A territory is owned by a player and contain a number of armies.
     int numberOfArmies;
 
 public:
-    Territory(std::string name, int coordinateX, int coordinateY, Continent* continent, std::vector<std::string> adjacentTerritoryNames);
+    Territory(string name, int coordinateX, int coordinateY, Continent* continent);
+    string getName();
+    void addAdjacentTerritory(Territory* territory);
 };
 
 #endif //TERRITORY_H
--- src/map/Territory.cpp
+++ src/map/Territory.cpp
@@ -3,16 +3,27 @@
 //
 
 #include <utility>
+#include <string>
+    using std::string;
 
 #include "../../include/Territory.h"
 
-Territory::Territory(std::string name, int coordinateX, int coordinateY, Continent* continent,
-                     std::vector<std::string> adjacentTerritoryNames)
+Territory::Territory(string name, int coordinateX, int coordinateY, Continent* continent)
 {
     this->name = std::move(name);
     this->coordinateX = coordinateX;
     this->coordinateY = coordinateY;
     this->continent = continent;
-    this->adjacentTerritoryNames = std::move(adjacentTerritoryNames);
+    this->adjacentTerritories = {};
     this->numberOfArmies = 0;
+}
+
+string Territory::getName()
+{
+    return this->name;
+}
+
+void Territory::addAdjacentTerritory(Territory *territory)
+{
+    this->adjacentTerritories.push_back(territory);
 }

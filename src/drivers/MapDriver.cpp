dc5f5c93b67c938b69f78ac9b7d0e78ebffa34f6
Rayan Moarkech <rayanzone@hotmail.com>
2022-10-06 22:41:15 -0400

    delete maps

--- src/drivers/MapDriver.cpp
+++ src/drivers/MapDriver.cpp
@@ -6,8 +6,23 @@
 
 void testLoadMaps()
 {
-    MapLoader::load("./001_I72_Ghtroc720.map");
-    MapLoader::load("./004_I72_Cobra.map");
-    MapLoader::load("./Antarctica.map");
-    MapLoader::load("./Cornwall.map");
+    Map* map = MapLoader::load("./001_I72_Ghtroc720.map");
+    delete map;
+    map = MapLoader::load("./004_I72_Cobra.map");
+    delete map;
+    map = MapLoader::load("./Antarctica.map");
+    delete map;
+    map = MapLoader::load("./APKomp.map");
+    delete map;
+    map = MapLoader::load("./Cornwall.map");
+    delete map;
+
+    map = MapLoader::load("./duplicate_continent.map");
+    delete map;
+    map = MapLoader::load("./duplicate_territory.map");
+    delete map;
+    map = MapLoader::load("./missing_continent.map");
+    delete map;
+    map = MapLoader::load("./missing_territory.map");
+    delete map;
 }

569d335b897f05adc0e6795d10587be29fa2570f
Rayan Moarkech <rayanzone@hotmail.com>
2022-10-09 15:14:38 -0400

    Update PlayerDriver.cpp

--- src/drivers/PlayerDriver.cpp
+++ src/drivers/PlayerDriver.cpp
@@ -1,14 +1,26 @@
+//
+// COMP345_PROJECT_PLAYERDRIVER_CPP PlayerDriver.cpp
+//
+
 #include "../../include/Map.h"
 #include "../../include/Player.h"
+
 #include <vector>
-using std::vector;
+    using std::vector;
 
 #include <iostream>
-using std::cout;
-using std::endl;
+    using std::cout;
+    using std::endl;
 
 void testPlayers()
 {
+    cout << endl
+        << "------------------------------------------------------"
+        << endl
+        << "Test Players" << endl
+        << "------------------------------------------------------" << endl
+        << endl;
+
 	srand(time(NULL)); //change seed to get random numbers every run
 	Player* player = new Player();
 	vector<Territory*> territoriesToDefend = player->toDefend();

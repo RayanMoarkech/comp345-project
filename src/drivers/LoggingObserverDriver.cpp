dd3d4b2c72c65cddc73b1e05d0e531a50a121e43
Rayan Moarkech <rayanzone@hotmail.com>
2022-11-16 21:08:20 -0500

    Update LoggingObserverDriver.cpp

--- src/drivers/LoggingObserverDriver.cpp
+++ src/drivers/LoggingObserverDriver.cpp
@@ -11,77 +11,76 @@
 using std::cout;
 using std::endl;
 
-void testLoggingObserver() {
+void testLoggingObserver()
+{
 
-        LogObserver* logger = new LogObserver();
+    LogObserver* logger = new LogObserver();
 
 #pragma region TestCommandProcessing
 
-        cout << "------------------ Test: COMMAND PROCESSOR ------------------" << endl;
+    cout << "------------------ Test: COMMAND PROCESSOR ------------------" << endl;
 
-        cout << "Creating CommandProcessor" << endl;
-        CommandProcessor* commandProcessor = new CommandProcessor();
+    cout << "Creating CommandProcessor" << endl;
+    CommandProcessor* commandProcessor = new CommandProcessor();
 //        commandProcessor->Attach(logger);
 
-        cout << endl << "Obtaining command: ";
-        Command* command = commandProcessor->getCommand();
+    cout << endl << "Obtaining command: ";
+    Command* command = commandProcessor->getCommand();
 
-        command->saveEffect("effect test");
+    command->saveEffect("effect test");
 
-//        delete commandProcessor;
+    delete commandProcessor;
 
 #pragma endregion
 
 #pragma region TestGameEngine
 
-        cout << endl << "------------------ Test: GAME ENGINE ------------------" << endl;
-        cout << endl << "Creating GameEngine" << endl;
-        GameEngine *gameEngine = new GameEngine("");
-        // attach logger to gameEngine here
-        //
+    cout << endl << "------------------ Test: GAME ENGINE ------------------" << endl;
+    cout << endl << "Creating GameEngine" << endl;
+    GameEngine *gameEngine = new GameEngine("");
+    commandProcessor = gameEngine->getCommandProcessor();
+    // attach logger to gameEngine here
+    //
 
-        delete gameEngine;
+    // Will log the new state of the game engine (MAP_LOADED)
+    cout << endl << "TRANSITION:  " << gameEngine->stringToLog() <<" to maploaded" << endl;
+    commandProcessor->saveCommand("loadmap Cornwall.map");
+    gameEngine->transition();
 
-        // Will log the new state of the game engine (MAP_LOADED)
-        cout << endl << "TRANSITION:  " << gameEngine->stringToLog() <<" to maploaded" << endl;
-        gameEngine->getState();
+    // Will log the new state of the game engine (MAP_VALIDATED)
+    cout << endl << "TRANSITION " << gameEngine->stringToLog() << " to mapvalidated" << endl;
+    commandProcessor->saveCommand("validatemap");
+    gameEngine->transition();
 
-        // Will log the new state of the game engine (MAP_VALIDATED)
-        cout << endl << "TRANSITION " << gameEngine->stringToLog() << " to mapvalidated" << endl;
-//        gameEngine->
-
-        delete gameEngine;
+    delete gameEngine;
 
 #pragma endregion
 
 #pragma region TestOrders
 
-    cout << endl
-         << "------------------------------------------------------" << endl
-         << "Test Orders List" << endl
-         << "------------------------------------------------------" << endl
-         << endl;
+    cout << endl << "------------------ Test: ORDERS ------------------" << endl;
+
+    Map* map = MapLoader::load("Cornwall.map");
 
     cout << "\nCreating an ordersList object:" << endl;
     OrdersList ordersList;
 
     cout << "\nCreating an order of each type:" << endl;
-    Deploy deployOrder;
-    Advance advanceOrder;
+    Deploy* deployOrder = new Deploy(new Player(), map->getTerritories()[0], 2);
+    Advance* advanceOrder = new Advance(new Player(), map->getTerritories()[0], map->getTerritories()[1], 2);
     Bomb bombOrder;
     Airlift airliftOrder;
     Blockade blockadeOrder;
     Negotiate negotiateOrder;
 
     cout << "\nPlacing orders in orders list object" << endl;
-    ordersList.addOrder(&deployOrder);
-    ordersList.addOrder(&advanceOrder);
+    ordersList.addOrder(deployOrder);
+    ordersList.addOrder(advanceOrder);
     ordersList.addOrder(&bombOrder);
     ordersList.addOrder(&airliftOrder);
     ordersList.addOrder(&blockadeOrder);
     ordersList.addOrder(&negotiateOrder);
     cout << ordersList << endl;
-
 
     cout << "Testing remove method" << endl;
     ordersList.remove(2);
@@ -92,13 +91,13 @@ void testLoggingObserver() {
     cout << ordersList << endl;
 
     cout << "Testing validate method." << endl;
-    if (advanceOrder.validate()) { cout << "Validate was successful" << endl; }
+    if (advanceOrder->validate()) { cout << "Validate was successful" << endl; }
 
     cout << "\nTesting execute method on validated order:" << endl;
-    advanceOrder.execute();
+    advanceOrder->execute();
 
     cout << "\nTesting execute method on unvalidated order:" << endl;
-    deployOrder.execute();
+    deployOrder->execute();
 
     cout << "\nTesting stream insertion operator of executed order:" << endl;
     cout << advanceOrder << endl;
@@ -106,8 +105,12 @@ void testLoggingObserver() {
     cout << "\nTesting stream insertion operator of unexecuted order:" << endl;
     cout << deployOrder << endl;
 
+    delete map;
+    delete advanceOrder;
+    delete deployOrder;
+
 #pragma endregion
 
-        delete logger;
-        std::cin.get();
-    }
+    delete logger;
+    std::cin.get();
+}
